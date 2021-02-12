283b135b44c3af856b9ad681eda83127a48d4c4e
JER.8789 <je.rogers8789@gmail.com>
2021-02-11 21:30:26 -0600

    created initial files

---
+++ script.js
@@ -0,0 +1,11 @@
+// I want a timed quiz on JS fundamentals that stores the highest scores to gauge progress against peers
+
+//I want a start button that when clicked will begin a timer and present a question
+
+// when I answer a question
+
+// present the next question
+
+// when a question is answered incorrectly time is subtracted from the clock
+
+// when all questions are answered or the timer reaches 0 the game is over
